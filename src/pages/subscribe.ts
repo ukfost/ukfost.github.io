5b12abb3bd55030120bfc3223f7fe1c4bb6c157a
Angus Findlay <angus.findlay@icloud.com>
2023-10-18 21:53:42 +0100

    key for the value

--- src/pages/subscribe.ts
+++ src/pages/subscribe.ts
@@ -4,7 +4,7 @@ export async function post({ request }) {
   const formData = await request.formData();
   const email = formData.get("email");
 
-  await kv.lpush(email);
+  await kv.lpush("emails", email);
 
   return new Response(null, {
     status: 302,
