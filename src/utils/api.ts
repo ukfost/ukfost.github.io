296286e04b484d9b5284081fe602b50ea6359427
Angus Findlay <angus.findlay@icloud.com>
2024-01-14 20:26:19 +0000

    paginate posts

---
+++ src/utils/api.ts
@@ -0,0 +1,23 @@
+type Post = {
+  slug: string;
+  title: string;
+  content: string;
+};
+
+export async function getPosts(): Promise<Post[]> {
+  const posts: Post[] = [];
+
+  for (let offset = 0; offset < 100; offset += 20) {
+    const resp = await fetch(
+      `https://public-api.wordpress.com/rest/v1.1/sites/ukfost.wordpress.com/posts?offset=${offset}`
+    );
+    const data = await resp.json();
+    if (data.posts.length) {
+      posts.push(...data.posts);
+    } else {
+      break;
+    }
+  }
+
+  return posts;
+}
