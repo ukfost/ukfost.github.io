a7b9a05ed7e62154f220dfd195cedf51119894d2
Angus Findlay <angus.findlay@icloud.com>
2024-01-16 22:35:59 +0000

    activists admin

---
+++ src/pages/admin/activists.ts
@@ -0,0 +1,33 @@
+import { kv } from "@vercel/kv";
+
+export async function get({ request }) {
+  const auth = request.headers.get("authorization");
+  if (auth && auth.startsWith("Basic ")) {
+    const encoded = auth.substring(6);
+    const decoded = atob(encoded);
+    const [username, password] = decoded.split(":");
+    if (
+      username.length > 5 &&
+      password.length > 5 &&
+      username === process.env.ADMIN_USERNAME &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const activists = await kv.lrange("activists", 0, -1);
+
+      return new Response(
+        [...new Set(activists.filter((activists) => activists != ""))].join(
+          "\n"
+        ),
+        {
+          status: 200,
+          headers: { "Content-Type": "text/plain" },
+        }
+      );
+    }
+  }
+
+  return new Response(null, {
+    status: 401,
+    headers: { "WWW-Authenticate": "Basic realm='password access required'" },
+  });
+}
